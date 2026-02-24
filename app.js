f04556f8eb61f3d7affb5acf7778d4bef18b6f0d
Roberto Gonzalez <rob.gonzalez.castro@gmail.com>
2026-02-24 18:54:23 -0500

    kanban pro v1

---
+++ app.js
@@ -0,0 +1,23 @@
+﻿const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+hbs.registerHelper("ifEquals", function(a, b, options) {
+  return a === b ? options.fn(this) : options.inverse(this);
+});
+
+app.get("/", (req, res) => {
+  const tareas = require("./tasks.json");
+  res.render("kanban", { tareas });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
